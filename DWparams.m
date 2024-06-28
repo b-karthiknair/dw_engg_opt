4a19366a9f24cd00d6fbc5a03d0f4fdae3735778
akansha2001 <66127211+akansha2001@users.noreply.github.com>
2024-06-28 14:08:28 +0530

    Updated params file compatible with multi-step
    added xg,yg and commented parts which are updated using edit_files after each time step (xi,yi,va,wa,thetai)

--- DWparams.m
+++ DWparams.m
@@ -14,20 +14,29 @@
 %   M : Large distance to be set if there are no obstacles on the robot
 %       trajectory (m).
 
-xi = 1.0;
-yi = 2.0;
-thetai = 0.25*pi;
-thetag = 0.4*pi;
+xi = 1.086642e+00;
+yi = 2.087188e+00;
+thetai = 7.919245e-01;
+va = 7.370502e-02;
+wa = 4.616200e-03;
+% xi = 1.0;
+% yi = 2.0;
+% thetai = 0.25*pi;
+% va = 0.3;
+% wa = 0.2*pi;
+
+xg = 5;
+yg = 4;
+thetag = atan2(yg,xg);
+
 obstacles = [[1.2,4.1];[1.1,2.1];[2.9,6.1];[4.2,1.1]];
 
-delt = 0.25;
-t_interval = delt/10;
+delt = 0.1;
+t = delt/10;
 M = 10;
 a = 2.0;
 b = 0.2;
 c = 0.2;
-va = 0.3;
-wa = 0.2*pi;
 v_min = 0;
 v_max = 0.9;
 w_min = -pi/2;
@@ -36,3 +45,12 @@ a_lin_b = [-0.1,0.2];
 a_ang_b = [-0.03,0.03];
 
 %end
+
+
+
+
+
+
+
+
+
